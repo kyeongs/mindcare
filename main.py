3305785756dceff9658a0fea212ee4b51d82faba
kyeongs <77399159+kyeongs@users.noreply.github.com>
2023-03-16 22:48:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
-import openpyxl as ox
+import pymysql
 
-dir = 'C:\\Users\\admin\\PycharmProjects\\web\\mindcare.xlsx'
-wb = ox.load_workbook('mindcare.xlsx')
-ws = wb.active
+db = pymysql.connect(host="localhost", user="root", password="root", charset="utf8")
+cursor = db.cursor()
+
 name = st.text_input('면담자 이름을 입력하세요', max_chars=10, key='name')
 
 date = st.date_input('면담 날짜')
@@ -12,8 +12,19 @@ time = st.time_input('면담 시간')
 
 story = st.text_area('면담 내용')
 
-#버튼 클릭 시 엑셀 파일에 저장
+# mySQL에 데이터 저장
 if st.button('저장'):
-    ws.title = 'mindcare'
-    ws.append([name, date, time, story])
-    wb.save('mindcare.xlsx')
+
+
+    query = """insert into tasks (user_name, start_date, start_time, story) values (%s, %s, %s, %s);"""
+    param = (name, date, time, story)
+    cursor.execute('USE users;')
+    cursor.execute(query, param)
+
+    cursor.execute('select * from tasks;')
+    result = cursor.fetchall()
+    for record in result:
+        print(record)
+
+    db.commit()
+    db.close()
