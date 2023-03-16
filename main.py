4cb0fb5ac9e8503da8143f26d79e4d6b7390f64c
kyeongs <77399159+kyeongs@users.noreply.github.com>
2023-03-16 21:00:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from openpyxl import load_workbook
 
+wb = load_workbook('mindcare.xlsx')
+ws = wb.active
 name = st.text_input('면담자 이름을 입력하세요', max_chars=10, key='name')
 
 date = st.date_input('면담 날짜')
@@ -9,4 +12,7 @@ time = st.time_input('면담 시간')
 story = st.text_area('면담 내용')
 
 #버튼 클릭 시 엑셀 파일에 저장
-
+if st.button('저장'):
+    ws.title = 'mindcare'
+    ws.append([name, date, time, story])
+    wb.save('mindcare.xlsx')
